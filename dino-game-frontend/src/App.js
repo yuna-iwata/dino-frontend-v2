0888a8969d445affadabb64d4a4eae254f4de8b9
louisglan <louissam@hotmail.co.uk>
2023-01-11 16:54:29 +0000

    added create account static page

--- dino-game-frontend/src/App.js
+++ dino-game-frontend/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GamePage from "./Pages/GamePage.js";
+import CreateAccountPage from "./Pages/CreateAccountPage.js";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/game" element={<GamePage />} />
+        <Route path="create-account" element={<CreateAccountPage />} />
       </Routes>
     </BrowserRouter>
   );
