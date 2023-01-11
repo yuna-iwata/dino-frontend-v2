71e42c0659d6c00d132d6362306d6228a01c4acc
louisglan <louissam@hotmail.co.uk>
2023-01-11 17:15:26 +0000

    created static login page

---
+++ dino-game-frontend/src/Pages/LoginPage.js
@@ -0,0 +1,19 @@
+import LoginForm from "../Components/LoginForm.js";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+
+export default function CreateAccountPage(props) {
+  const { changeUser } = props;
+  return (
+    <div className="container">
+      <h1>Login</h1>
+      <LoginForm changeUser={changeUser} />
+      <p>Don't have an account?</p>
+      <Link style={{ textDecoration: "none" }} to="/create-account">
+        <Button variant="outlined" color="inherit">
+          Sign Up
+        </Button>
+      </Link>
+    </div>
+  );
+}
