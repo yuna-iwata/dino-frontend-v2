5b4279cb8dd5d5db507ad3a5b329f79046e443ce
louisglan <louissam@hotmail.co.uk>
2023-01-12 12:57:46 +0000

    added dino image

--- src/Pages/WelcomePage.jsx
+++ src/Pages/WelcomePage.jsx
@@ -13,27 +13,30 @@ export default function WelcomePage() {
     },
   });
   return (
-    <div className="stack-container">
-      <div className="welcome-page-container">
-        <Stack direction="column" spacing={5} alignItems="center">
-          <ThemeProvider theme={theme}>
-            <Link style={{ textDecoration: "none" }} to="/create-account">
-              <Button variant="contained" color="green">
-                Create Account
-              </Button>
-            </Link>
-            <Link style={{ textDecoration: "none" }} to="/login">
-              <Button variant="contained" color="green">
-                Log In
-              </Button>
-            </Link>
-            <Link style={{ textDecoration: "none" }} to="/game">
-              <Button variant="contained" color="green">
-                Continue As Guest
-              </Button>
-            </Link>
-          </ThemeProvider>
-        </Stack>
+    <div className="container">
+      <img src="/dinobg.jpg" alt="dino-bg" />
+      <div className="stack-container">
+        <div className="welcome-page-container">
+          <Stack direction="column" spacing={5} alignItems="center">
+            <ThemeProvider theme={theme}>
+              <Link style={{ textDecoration: "none" }} to="/create-account">
+                <Button variant="contained" color="green">
+                  Create Account
+                </Button>
+              </Link>
+              <Link style={{ textDecoration: "none" }} to="/login">
+                <Button variant="contained" color="green">
+                  Log In
+                </Button>
+              </Link>
+              <Link style={{ textDecoration: "none" }} to="/game">
+                <Button variant="contained" color="green">
+                  Continue As Guest
+                </Button>
+              </Link>
+            </ThemeProvider>
+          </Stack>
+        </div>
       </div>
     </div>
   );
