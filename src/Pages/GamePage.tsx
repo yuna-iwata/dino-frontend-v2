e787e48c02243ae3ae1f619bd0f42930f15ebef2
Yuna Iwata <yunaiwata@Yunas-MacBook-Pro.local>
2023-01-16 11:23:50 +0000

    game not rerendering when score changed YAY

--- src/Pages/GamePage.tsx
+++ src/Pages/GamePage.tsx
@@ -1,26 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DinoGameScene from "../scenes/DinoGameScene";
-//
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  pixelArt: true,
+  transparent: true,
+  //autoCenter: true,
+  parent: "game",
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  width: 1000,
+  height: 300,
+  scene: [DinoGameScene],
+};
+const game = new Phaser.Game(config);
 
 export default function Gamepage() {
-  const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    pixelArt: true,
-    transparent: true,
-    //autoCenter: true,
-    parent: "game",
-    physics: {
-      default: "arcade",
-      arcade: {
-        debug: true,
-      },
-    },
-    width: 1000,
-    height: 300,
-    scene: [DinoGameScene],
-  };
-  const game = new Phaser.Game(config);
-
   const [score, setScore] = useState(0);
   //let scene = game.scene.keys.helloworld as HelloWorldScene;
 
@@ -34,7 +33,7 @@ export default function Gamepage() {
   return (
     <div>
       <h1> Game Page</h1>
-      <h3>score: {score}</h3>
+      <h3>score:{score}</h3>
       <button onClick={handleClick}>get score</button>
     </div>
   );
